84637f925cf9f46860faa717f36db98b11913399
Emil Söderling <asabla@users.noreply.github.com>
2025-02-09 18:35:40 +0100

    Added support for auth-stuff

---
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -0,0 +1,10 @@
+import { SignInForm } from '@/components/sign-in-form';
+
+export default function SignInPage() {
+  return (
+    <main aria-label='Sign in page' className='text-white'>
+      <h1>Welcome to Next-15-Azure-Auth</h1>
+      <SignInForm />
+    </main>
+  );
+}
--- src/services/extractors/news-article.extractor.ts
+++ src/services/extractors/news-article.extractor.ts
@@ -103,7 +103,7 @@ function isValidUrl(urlString: string): boolean {
     try {
         new URL(urlString);
         return true;
-    } catch (e) {
+    } catch {
         return false;
     }
 }
